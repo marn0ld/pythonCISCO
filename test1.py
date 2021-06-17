a5bcd8adc3ce6d451152fab5cb294235f9d249b5
arno <arnomuke1@gmail.com>
2021-06-17 12:57:12 +0300

    DRUGE ZAVDANYA

--- test1.py
+++ test1.py
@@ -1,22 +1,50 @@
+#на вхід приймає рядок
+q = 'arno31 muke231 mogas'
 
-secret_number = 777
 
-print(
-    """
-+================================+
-| Welcome to my game, muggle!    |
-| Enter an integer number        |
-| and guess what number I've     |
-| picked for you.                |
-| So, what is the secret number? |
-+================================+
-""")
-user_number = int(input('Im choosing number...'))
-while secret_number != user_number:
-    if secret_number == user_number:
-        print("Well done, muggle! You are free now")
+def stringcheck(s):
+    sep = s.split()
+    sep2 = " ".join(sep)
+    print("-" * 45)
+    print("String:", sep2)
 
-    else:
-        print("Ha ha! Youre stuck in my loop")
+    nums = []
+    letters = []
 
-    user_number = int(input('Im choosing number again...'))
+    for el in list(q):
+        if el.isdigit(): #виділяє з нього всі числа в окремий масив
+            nums.append(el) 
+            nums2 = "".join(nums)
+        else:
+            letters.append(el)
+            letters2 = "".join(letters)
+    print("-"*45)
+    #після чого програма друкує рядок без чисел і масив чисел
+    print("List with numbers:", int(nums2))
+    print("List without numbers:", letters2)
+
+    print("-" * 60)
+    #Змінити цей рядок таким чином, щоб кожне слово в ньому, починалось і закінчувалось великою літерою
+    letterers = []
+    letters2, letters3 = letters2.title(), ""
+    for i in letters2.split():
+        letters3 += i[:-1] + i[-1].upper() + " "
+        letterers.append(letters3[:-1])
+    print("First and last letters capitalized:", letterers[-1])
+
+    print("-" * 60)
+    #Знайти максимальне значення в масиві чисел
+    print("Max number in a number array:", int(max(nums2)))
+    #всі інші числа піднести до степеню по їх індексу, та записати в інший масив
+    res = []
+    for el2 in list(nums2):
+        if int(el2) != int(max(nums2)):
+            power = int(el2) ** int(el2)
+            res.append(power)
+
+    print("Self-powered numbers:", res)
+
+    print("-" * 60)
+
+
+stringcheck(q)
